c4944b54a244fa4fe853cee341bfe52e46425024
Hudu <ahmedhudu21@gmail.com>
2023-10-21 02:55:11 +0200

    style/nav cclear

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
-import rick from '../../../public/images/rick.png';
-import './styles.scss';
+import rick from "../../assets/images/rick.png";
+import "./styles.scss";
 
 const Navbar = () => {
   return (
     <nav className="nav-container">
       <ul>
-        <li><Link to='/'>
-          <img src={rick} alt="image-rick" className="logo" />
-          </Link></li>
-        <li><Link to='/characters'>Characters</Link></li>
-        <li><Link to='/about'>About</Link></li>
+        <li>
+          <Link to="/">
+            <img src={rick} alt="image-rick" className="logo" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/characters" className="character">
+            Characters
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className="about">
+            About
+          </Link>
+        </li>
       </ul>
     </nav>
   );

51f89faffac7afca11c848c6a38146e1121e24f7
Hudu <ahmedhudu21@gmail.com>
2023-10-21 14:05:42 +0200

    create header text

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import "./style.scss";
+const Header = () => {
+  return (
+    <div className="text-container">
+      <div className="text">Rick y Morty</div>
+    </div>
+  );
+};
+
+export default Header;

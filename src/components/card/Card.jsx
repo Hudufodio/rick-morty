c42ad59357dd218e44df0092c923c3dd4c396072
Hudu <ahmedhudu21@gmail.com>
2023-10-22 02:37:53 +0200

    style card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,18 +1,78 @@
-import './style.scss';
+import "./style.scss";
+import beach from "../../assets/images/beach.jpg";
+
 const Card = () => {
   return (
-    <div className="card">
+    <div>
       <p className="characters">List of characters</p>
-      <div className="card-container">
-        <div className="section-image">
-          <img src="" alt="" />
+      <div className="card">
+        <div className="card-container">
+          <div className="section-image">
+            <img src={beach} alt="image of rick" />
+          </div>
+          <h3 className="text-name">Morty Smith</h3>
+          <span className="text-human">Human</span>
+          <h4 className="text-location">Earth</h4>
         </div>
-        <h3 className="text-name">Morty Smith</h3>
-        <span className="text-human">Human</span>
-        <h4 className="text-location">Earth</h4>
+        <div className="card-container">
+          <div className="section-image">
+            <img src={beach} alt="image of rick" />
+          </div>
+          <h3 className="text-name">Morty Smith</h3>
+          <span className="text-human">Human</span>
+          <h4 className="text-location">Earth</h4>
+        </div>
+        <div className="card-container">
+          <div className="section-image">
+            <img src={beach} alt="image of rick" />
+          </div>
+          <h3 className="text-name">Morty Smith</h3>
+          <span className="text-human">Human</span>
+          <h4 className="text-location">Earth</h4>
+        </div>
+        <div className="card-container">
+          <div className="section-image">
+            <img src={beach} alt="image of rick" />
+          </div>
+          <h3 className="text-name">Morty Smith</h3>
+          <span className="text-human">Human</span>
+          <h4 className="text-location">Earth</h4>
+        </div>
+        <div className="card-container">
+          <div className="section-image">
+            <img src={beach} alt="image of rick" />
+          </div>
+          <h3 className="text-name">Morty Smith</h3>
+          <span className="text-human">Human</span>
+          <h4 className="text-location">Earth</h4>
+        </div>
+        <div className="card-container">
+          <div className="section-image">
+            <img src={beach} alt="image of rick" />
+          </div>
+          <h3 className="text-name">Morty Smith</h3>
+          <span className="text-human">Human</span>
+          <h4 className="text-location">Earth</h4>
+        </div>
+        <div className="card-container">
+          <div className="section-image">
+            <img src={beach} alt="image of rick" />
+          </div>
+          <h3 className="text-name">Morty Smith</h3>
+          <span className="text-human">Human</span>
+          <h4 className="text-location">Earth</h4>
+        </div>
+        <div className="card-container">
+          <div className="section-image">
+            <img src={beach} alt="image of rick" />
+          </div>
+          <h3 className="text-name">Morty Smith</h3>
+          <span className="text-human">Human</span>
+          <h4 className="text-location">Earth</h4>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
